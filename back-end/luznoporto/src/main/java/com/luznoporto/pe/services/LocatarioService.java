0ad7da569377663823f6e449c6b97d99d8a9af5f
Hennan Gadelha <hennanhennan@gmail.com>
2021-05-09 19:52:16 -0300

    Adicionando metodo no service para listar todos usuarios

--- back-end/luznoporto/src/main/java/com/luznoporto/pe/services/LocatarioService.java
+++ back-end/luznoporto/src/main/java/com/luznoporto/pe/services/LocatarioService.java
@@ -1,5 +1,24 @@
 package com.luznoporto.pe.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luznoporto.pe.models.Locatario;
+import com.luznoporto.pe.repositories.LocatarioRepository;
+
+@Service
 public class LocatarioService {
 
+	@Autowired
+	LocatarioRepository locatarioRepository;
+	
+	
+	public List<Locatario> listarLocatarios(){
+		
+		return locatarioRepository.findAll();
+		
+	}
+	
 }
