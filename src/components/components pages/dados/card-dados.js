eb68bd2dc3e44c6fefc3d3375c9c6d9ef761a479
Marilia Vilar <mariliavilarpr@gmail.com>
2021-06-09 10:11:14 -0300

    tabela dados - resultado das requisicoes ja tratadas dos cards consumo e reducao - funcionando- e coluna da tabela.

--- src/components/components pages/dados/card-dados.js
+++ src/components/components pages/dados/card-dados.js
@@ -1,40 +1,128 @@
-import React from 'react';
-import { Card, CardTitle, CardText, CardDeck, CardBody } from 'reactstrap';
+import React, { Component } from 'react';
+import api from '../../../api';
+// import Moment from 'moment';
+// import { Card, CardTitle, CardText, CardDeck, CardBody } from 'reactstrap';
 
-const Card1 = (props) => {
-  return (
-    // <div className="card bg-light text-dark m-3">
-    // <div id="chart" className="card-body">
-    <CardDeck>
-      {/* <Card style={{ width: '20rem' }}> */}
-      <Card className="card bg-light text-dark m-3">
-        <CardBody>
-          <CardTitle tag="h5" style={{ textAlign: 'center' }}>
-            Consumo Total
-          </CardTitle>
-          <CardText>5.687kWh</CardText>
-        </CardBody>
-      </Card>
-      {/* <Card style={{ width: '20rem', height: '8rem' }}> */}
-      <Card className="card bg-light text-dark m-3">
-        <CardBody>
-          <CardTitle tag="h5" style={{ textAlign: 'center' }}>
-            Redução de Consumo
-          </CardTitle>
-          <CardText>1245kWh</CardText>
-        </CardBody>
-      </Card>
-      {/* <Card style={{ width: '20rem' }}> */}
-      <Card className="card bg-light text-dark m-3">
-        <CardBody>
-          <CardTitle tag="h5" style={{ textAlign: 'center' }}>
-            Redução de Emissões
-          </CardTitle>
-          <CardText>----t/CO2</CardText>
-        </CardBody>
-      </Card>
-    </CardDeck>
-  );
-};
+class Card1 extends Component {
+  constructor() {
+    super();
 
+    var anoAtual = new Date().getFullYear();
+    var mesAtual = new Date().getMonth() + 1;
+    var ultimoMes = new Date().getMonth().toString();
+    var penultimoMes = (new Date().getMonth() - 1).toString();
+    var antepenultimoMes = (new Date().getMonth() - 2).toString();
+    if (ultimoMes.length === 1) {
+      ultimoMes = '0' + ultimoMes;
+    }
+    if (penultimoMes.length === 1) {
+      penultimoMes = '0' + penultimoMes;
+    }
+    if (antepenultimoMes.length === 1) {
+      antepenultimoMes = '0' + antepenultimoMes;
+    }
+
+    var periodoUltimoConsumo = `${anoAtual}-${penultimoMes}-01/${anoAtual}-${ultimoMes}-01`;
+    var periodoPenultimoConsumo = `${anoAtual}-${antepenultimoMes}-01/${anoAtual}-${penultimoMes}-01`;
+
+    this.state = {
+      periodoUltimoConsumo: periodoUltimoConsumo,
+      periodoPenultimoConsumo: periodoPenultimoConsumo,
+      ultimoConsumoTotal: [],
+      penultimoConsumoTotal: [],
+      reducaoConsumo: [],
+      status: [],
+    };
+  }
+
+  async componentDidMount() {
+    const responseUltimoConsumo = await api.get(
+      `/medicoes/buscarPorPeriodo/${this.state.periodoUltimoConsumo}`,
+    );
+    console.log('responseUltimoConsumo', responseUltimoConsumo.data);
+    this.setState({
+      ultimoConsumoTotal: responseUltimoConsumo.data.valorTotalDoPeriodo,
+    });
+
+    const responsePenultimoConsumo = await api.get(
+      `/medicoes/buscarPorPeriodo/${this.state.periodoPenultimoConsumo}`,
+    );
+    console.log('responsePenultimoConsumo', responsePenultimoConsumo.data);
+    this.setState({
+      penultimoConsumoTotal: responsePenultimoConsumo.data.valorTotalDoPeriodo,
+    });
+
+    this.setState({
+      reducaoConsumo: (
+        ((parseFloat(this.state.ultimoConsumoTotal) -
+          parseFloat(this.state.penultimoConsumoTotal)) /
+          parseFloat(this.state.penultimoConsumoTotal)) *
+        100
+      ).toFixed(2),
+    });
+
+    if (this.state.reducaoConsumo > 0) {
+      this.setState({
+        status: 'aumento',
+      });
+    } else if (this.state.reducaoConsumo < 0) {
+      this.setState({
+        status: 'redução',
+      });
+    } else {
+      this.setState({
+        status: 'estável',
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>ultimo consumo: {this.state.ultimoConsumoTotal}</p>
+        <p>penultimo consumo: {this.state.penultimoConsumoTotal}</p>
+        <p>status: {this.state.status}</p>
+        <p>%perc: {this.state.reducaoConsumo}%</p>
+      </div>
+    );
+  }
+}
 export default Card1;
+
+// const Card1 = (props) => {
+//   return (
+//     // <div className="card bg-light text-dark m-3">
+//     // <div id="chart" className="card-body">
+//     <CardDeck>
+//       {/* <Card style={{ width: '20rem' }}> */}
+//       <Card className="card bg-light text-dark m-3">
+//         <CardBody>
+//           <CardTitle tag="h5" style={{ textAlign: 'center' }}>
+//             Consumo Total
+//           </CardTitle>
+//           <CardText>5.687kWh</CardText>
+//         </CardBody>
+//       </Card>
+//       {/* <Card style={{ width: '20rem', height: '8rem' }}> */}
+//       <Card className="card bg-light text-dark m-3">
+//         <CardBody>
+//           <CardTitle tag="h5" style={{ textAlign: 'center' }}>
+//             Redução de Consumo
+//           </CardTitle>
+//           <CardText>1245kWh</CardText>
+//         </CardBody>
+//       </Card>
+//       {/* <Card style={{ width: '20rem' }}> */}
+//       <Card className="card bg-light text-dark m-3">
+//         <CardBody>
+//           <CardTitle tag="h5" style={{ textAlign: 'center' }}>
+//             Redução de Emissões
+//           </CardTitle>
+//           <CardText>----t/CO2</CardText>
+//         </CardBody>
+//       </Card>
+//     </CardDeck>
+//   );
+// };
+
+// export default Card1;
